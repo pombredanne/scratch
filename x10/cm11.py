3cd6d33dd25e4f080b94d4421350585baafa94a9
Tristan Hill <stan@saticed.me.uk>
2009-07-19 22:17:02 +0100

    * simple x10 command line script (requires ControlX10-CM11 from cpan)

---
+++ x10/cm11.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import os
+import subprocess
+import re
+import sys
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+def main():
+    args = sys.argv[1:]
+    if len(args) == 2:
+        send_x10(*args)
+        sys.exit(0)
+    sys.exit("invalid args %s" % args)
+
+
+address_re = re.compile("^(?P<housecode>[A-F])[1-9]$")
+commands_map = {"on": "J", "off": "K"}
+def send_x10(address, command):
+    address = address.upper()
+    command = command.lower()
+    m = address_re.match(address)
+    if m is not None and command in commands_map:
+        cmd = ["perl", "-I", DIR, os.path.join(DIR, "eg", "eg_cm11.plx"),
+            "/dev/ttyUSB0"]
+        p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+        p.stdin.write("%s\n" % address)
+        p.stdin.write("%s%s\n" % (m.group("housecode"),
+            commands_map[command]))
+        p.stdin.close()
+        p.wait()
+        return
+    else:
+        raise ValueError("invalid args %s" % (address, command))
+
+
+if __name__ == "__main__":
+    main()
