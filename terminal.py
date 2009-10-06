3b8542bb285616c37a7c4109cae8711ae8895b53
Tristan Hill <stan@saticed.me.uk>
2009-10-06 18:38:41 +0100

    * exit on good sub command return

--- terminal.py
+++ terminal.py
@@ -11,10 +11,18 @@ def vte_commit(term, arg1, arg2):
     if arg1 == "\r":
         gtk.main_quit()
 
-  
+
+def on_child_exit(term):
+    status = term.get_child_exit_status()
+    if os.WIFEXITED(status) and os.WEXITSTATUS(status) == 0:
+        gtk.main_quit()
+    else:
+        term.connect("commit", vte_commit)
+
+
 if __name__ == "__main__":
     term = vte.Terminal()
-    term.connect("child-exited", lambda term: term.connect("commit", vte_commit))
+    term.connect("child-exited", on_child_exit)
     term.fork_command(sys.argv[1], sys.argv[1:])
     window = gtk.Window()
     window.add(term)
